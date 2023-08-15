832ec58bfcacddcb303788ba05c05a0b3e6a1208
JorgeCoto98 <120097112+JorgeCoto98@users.noreply.github.com>
2023-08-15 02:14:18 +0000

    Listo

--- src/app.py
+++ src/app.py
@@ -15,7 +15,17 @@ def hello_world():
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+
+    todos.append(request_body)
+    return jsonify(todos)
+    
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if 0 <= position < len(todos):
+        del todos[position]
+        return jsonify(todos), 200
+    else:
+        return jsonify({"error": "Invalid position"}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
